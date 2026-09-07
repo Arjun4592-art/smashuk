9b8c96542b9b356bddbef857dca9a5800575e57a
creative-crew98 <crewcreative98@gmail.com>
2026-09-07 11:53:38 +0530

    proxy Add

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 const nextConfig: NextConfig = {
   serverExternalPackages: ['isomorphic-dompurify', 'jsdom'],
+  turbopack: {
+    root: path.join(__dirname),
+  },
 }
 export default nextConfig
